3bbb677685cfb5a4f6c4243e89c00c45ee433f79
flatypus [mac] <yhc3141@gmail.com>
2023-12-06 00:39:16 -0800

    fix: read() reads from file its called from

--- aochelpers/read.py
+++ aochelpers/read.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 
 def read(relative="", filename="input.txt") -> str:
-    """Reads a file from the same directory as the current file, or with a relative path."""
-    file = sys.argv[0] if __name__ == "__main__" else __file__
+    """Reads a file from the same directory as the file this function is called from."""
+    file = sys.argv[0]
     current_dir = Path(file).parent
     path = current_dir / relative / filename
     with open(path, 'r') as f:
